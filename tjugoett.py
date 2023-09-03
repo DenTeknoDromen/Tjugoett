4d1bc3c73605e35a3dcc03509eee471bba5c40b1
Tobias Jansson <tobias.janssn@gmail.com>
2023-09-03 20:53:13 +0200

    La till parse funktion och ändrade lists till dict

--- tjugoett.py
+++ tjugoett.py
@@ -1,20 +1,30 @@
 import random
 import time
 
-color = ["Hjärter","Ruter","Klöver","Spader"]
-kortnamn =["Två","Tre","Fyra","Fem","Sex","Sju",
-           "Åtta","Nio","Tio","Knekt","Dam","Kung","Ess"]
+color = {1: "Hjärter", 2: "Ruter", 3: "Klöver", 4: "Spader"}
+kortnamn = {2: "Två", 3: "Tre", 4: "Fyra", 5: "Fem", 6: "Sex", 7: "Sju",
+            8: "Åtta", 9: "Nio", 10: "Tio", 11: "Knekt", 12: "Dam",
+            13: "Kung", 14: "Ess"}
 pile = []
 yourpile = []
 cpupile = []
 score = 0
 cpuscore = 0
 
+
+def parse(opt1, opt2, inpt):
+    if inpt == opt1 or inpt == opt2:
+        return inpt
+    else:
+        return parse(opt1, opt2, input(f"Skriv antingen "
+                                       f"{opt1} eller {opt2}: "))
+
+
 def drakort(humancheck, currpile, currscore):
     more = "y"
     while more == "y":
-        value = random.randint(2,14)       
-        kort = f"{color[random.randint(0,3)]} {kortnamn[value-2]}"
+        value = random.randint(2, 14)
+        kort = f"{color[random.randint(1,4)]} {kortnamn[value]}"
         if kort in pile:
             continue
         pile.append(kort)
@@ -22,22 +32,26 @@ def drakort(humancheck, currpile, currscore):
         print(f"{kort} \n")
         if value == 14:
             if humancheck:
-                value = int(input("Du fick ett Ess! Välj värde genom att skriva 1 eller 14: "))
+                value = int(parse("1", "14", (input("Du fick ett Ess! Välj "
+                                                    "värde genom att skriva 1 "
+                                                    "eller 14: "))))
             elif humancheck == 0:
-                value = 1     
+                value = 1
         currscore += value
         if currscore > 21:
             break
         if humancheck:
-            more = input("Vill du dra ett till kort? y/n: ").casefold()              
+            more = parse("y", "n", input("Vill du dra ett till "
+                                         "kort? y/n: ").casefold())
         elif currscore >= 14:
-                more = "n"
+            more = "n"
         time.sleep(1)
     return currscore
 
+
 print("--------------------")
-print("Din tur: \n")
-score = drakort(bool(1),yourpile, score)
+print("Din tur:")
+score = drakort(bool(1), yourpile, score)
 print("--------------------")
 if score > 21:
     print("Du förlorade!")
@@ -46,7 +60,7 @@ print(f"Här är dina kort: {yourpile}")
 time.sleep(2)
 print("--------------------")
 print("Datorns tur: \n")
-cpuscore = drakort(bool(0),cpupile, cpuscore)
+cpuscore = drakort(bool(0), cpupile, cpuscore)
 print("--------------------")
 print(f"Här är datorns poäng: {cpuscore}")
 print(f"Här är datorns kort: {cpupile}")
@@ -56,9 +70,3 @@ if score <= 21 and score > cpuscore:
     print("Du vann! Grattis!")
 else:
     print("Datorn vann")
-
-
-
-
-
-
